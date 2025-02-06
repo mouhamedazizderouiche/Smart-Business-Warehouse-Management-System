0877617dd61405eb2b93eb9268fbdf08c479bcc2
aminebichiou <aminebichiou2@gmail.com>
2025-02-06 23:20:34 +0100

    added navbar + concept of route

---
+++ PIDEV/src/Controller/LoginPageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginPageController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function index(): Response
+    {
+        $data = 'data';
+
+        return $this->render('login/loginform.html.twig', ['data' => $data]);
+    }
+}
