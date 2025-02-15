c8b8e12f398dafda8a736129593d0763580c97fc
Ilyes Chrif <ilyeschrif@outlook.com>
2025-02-15 22:39:17 +0100

    produitEntity&frontupdate&servicesupdate

---
+++ src/Form/ProductType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use App\Entity\Produit;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom du produit',
+            ])
+            ->add('prixUnitaire', TextType::class, [
+                'label' => 'Prix Unitaire',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+            ])
+            ->add('urlImageProduit', FileType::class, [
+                'label' => 'URL de l\'image',
+                'attr' => ['accept' => 'image/*'],
+            ])
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'nom',
+                'label' => 'Catégorie',
+                'placeholder' => 'Sélectionner une catégorie',
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'SUBMIT',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+            'is_edit' => false,
+        ]);
+    }
+}
