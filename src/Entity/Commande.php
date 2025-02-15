b7abd7ce6ab267cc059bc39c49b6f2bef3a71e83
zahia <mohamedaziz.zahi@edu.ece.fr>
2025-02-15 23:02:49 +0100

    Ajout de l'entité Commande avec UUID et relation Produit

---
+++ src/Entity/Commande.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+#[ORM\Entity(repositoryClass: CommandeRepository::class)]
+class Commande
+{
+    #[ORM\Id]
+    #[ORM\Column(type: "uuid", unique: true)]
+    private ?Uuid $id = null;
+
+    #[ORM\ManyToOne(targetEntity: Produit::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Produit $produit = null;
+
+    #[ORM\Column(type: "integer")]
+    private int $quantite = 1;
+
+    #[ORM\Column(type: "datetime")]
+    private \DateTimeInterface $dateCommande;
+
+    public function __construct()
+    {
+        $this->id = Uuid::v4();
+        $this->dateCommande = new \DateTime();
+    }
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+
+    public function getProduit(): ?Produit
+    {
+        return $this->produit;
+    }
+
+    public function setProduit(?Produit $produit): self
+    {
+        $this->produit = $produit;
+        return $this;
+    }
+
+    public function getQuantite(): int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): self
+    {
+        $this->quantite = $quantite;
+        return $this;
+    }
+
+    public function getDateCommande(): \DateTimeInterface
+    {
+        return $this->dateCommande;
+    }
+}
