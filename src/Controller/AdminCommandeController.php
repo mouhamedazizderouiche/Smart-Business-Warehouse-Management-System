7cf7029bc79e2936ae17fa62a8390d9ecdcaec38
amine <aminebichiou2@gmail.com>
2025-02-20 18:35:07 +0100

    after validation crud

--- src/Controller/AdminCommandeController.php
+++ src/Controller/AdminCommandeController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin', name: 'admin_')]
 class AdminCommandeController extends AbstractController
 {
     #[Route('/commandes', name: 'commandes')]
